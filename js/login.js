5b5bd77f0e13d949c8e998a62c5712a912edd342
gabrieltoledoo <rm551654@fiap.com.br>
2023-05-18 20:18:49 -0300

    refatoração dos elementos

--- js/login.js
+++ js/login.js
@@ -1,5 +1,10 @@
 //recuperando o input após receber o focus
 const inputUser = documen.querySelector("input[type='text']")
+const inputPass = document.querySelector("input[name='txtPass']");
+const labelUser = document.querySelector("label[for='idNm']");
+const inputSenha = document.querySelector("#idSenha");
+const labelSenha = document.querySelector("label[for='idSenha']");
+const labelUsuario = document.querySelector("label[for='idUsuario']");
 //Adicionando o evento de focus no inputUser
 //inputUser.addEventListener("focus", ()=>{
     //inputUser.setAttribute("NOME DO ATRIBUTO", "VALOR DO ATRIBUTO");
@@ -9,7 +14,6 @@ const inputUser = documen.querySelector("input[type='text']")
 
 inputUser.addEventListener("keyup", ()=>{
     //enquanto o usuário digitar conta-se a quantidade de caracteres que existem no campo, enquano a quantidade for menor do q 5, manteremos o outline na cor vermelha, assim que a quantidade atingir 5 ou mais mudamos a cor para verde.
-    const labelUser = document.querySelector("label[for='idNm']");
 
     if(inputUser.value.length < 5){
         inputUser.setAttribute("style","outline-color:#ff0000;");
@@ -20,3 +24,17 @@ inputUser.addEventListener("keyup", ()=>{
     }
 
 } );
+
+//Adicionando um evento click ao icone de olho!
+const eyePass = document.querySelector(".fa-eye");
+
+eyePass.addEventListener("click", ()=>{
+
+    if(inputPass.getAttribute("type") == "password"){
+        inputPass.setAttribute("type","text");
+    }else{
+        inputPass.setAttribute("type","password");
+    }
+    
+});
+
